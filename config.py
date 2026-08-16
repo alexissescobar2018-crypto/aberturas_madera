1741a8bc7f5ac49f33e7d6105582e88e4a111080
alexissescobar2018-crypto <alexissescobar2018@gmail.com>
2026-08-16 13:50:12 -0300

    Refactor Config class to lowercase and update key

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
 import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-aberturas'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///aberturas.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "clave-secreta-de-prueba"
